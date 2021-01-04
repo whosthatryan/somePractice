771f95ccca68c69e9d21aacc92411f1daa4f89e7
Ryan McLaughlin <itsryanmclaughlin@gmail.com>
2021-01-04 09:41:07 -0500

    recursion solutions

--- bigO.js
+++ bigO.js
@@ -625,3 +625,91 @@ const con = console.log;
 //     }
 //     return newArr;
 // }
+
+// capitalizeWords
+
+// const capitalizeWords = (array) => {
+//     if (array.length === 1) {
+//         return [array[0].toUpperCase()];
+//     }
+//     let res = capitalizeWords(array.slice(0, -1));
+//     res.push(array.slice(array.length-1)[0].toUpperCase());
+//     return res;
+// }
+
+// nestedEvenSum
+
+// function nestedEvenSum (obj, sum=0) {
+//     for (var key in obj) {
+//         if (typeof obj[key] === 'object'){
+//             sum += nestedEvenSum(obj[key]);
+//         } else if (typeof obj[key] === 'number' && obj[key] % 2 === 0){
+//             sum += obj[key];
+//         }
+//     }
+//     return sum;
+// }
+
+
+// capitalizeFirst 
+
+// const capitalizeFirst = (array) => {
+//     if (array.length === 1) {
+//       return [array[0][0].toUpperCase() + array[0].substr(1)];
+//     }
+//     const res = capitalizeFirst(array.slice(0, -1));
+//     const string = array.slice(array.length - 1)[0][0].toUpperCase() + array.slice(array.length-1)[0].substr(1);
+//     res.push(string);
+//     return res;
+//   }
+
+// stringifyNumbers
+
+// const stringifyNumbers = (obj) => {
+//     let newObj = {};
+//     for (let key in obj) {
+//       if (typeof obj[key] === 'number') {
+//         newObj[key] = obj[key].toString();
+//       } else if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
+//         newObj[key] = stringifyNumbers(obj[key]);
+//       } else {
+//         newObj[key] = obj[key];
+//       }
+//     }
+//     return newObj;
+//   }
+
+// collectStrings (helper method recursion)
+
+// const collectStrings = (obj) => {
+//     let stringsArr = [];
+ 
+//     function gatherStrings(o) {
+//         for (let key in o) {
+//             if (typeof o[key] === 'string') {
+//                 stringsArr.push(o[key]);
+//             }
+//             else if (typeof o[key] === 'object') {
+//                 return gatherStrings(o[key]);
+//             }
+//         }
+//     }
+ 
+//     gatherStrings(obj);
+ 
+//     return stringsArr;
+// }
+
+// collectStrings (pure recursion)
+
+// const collectStrings = (obj) => {
+//     let stringsArr = [];
+//     for (let key in obj) {
+//         if (typeof obj[key] === 'string') {
+//             stringsArr.push(obj[key]);
+//         } else if (typeof obj[key] === 'object') {
+//             stringsArr = stringsArr.concat(collectStrings(obj[key]));
+//         }
+//     }
+//     return stringsArr;
+// }
